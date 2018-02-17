ed51c9aaf59cbd8609dd44a383ec7536b0b70cf0
reiosantos <ronireiosantos@gmail.com>
2018-02-17 21:21:29 +0300

    commented out the added header to solve cross origin header error

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,16 +1,17 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		// add authorization header with jwt token if available
-		const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+		const currentUser = JSON.parse(localStorage.getItem(environment.userStorageKey));
 		if (currentUser && currentUser.token) {
 			request = request.clone({
 				setHeaders: {
-					Authorization: `Bearer ${currentUser.token}`
+					// Authorization: `Bearer ${currentUser.token}`
 				}
 			});
 		}
